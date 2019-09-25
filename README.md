e63b7a716f8827589fea95da91bbf68c87a7d90b
Lucas Bernardi <45859300+LucasdRossi@users.noreply.github.com>
2019-09-25 15:29:47 -0300

    adding video support

--- README.md
+++ README.md
@@ -1,19 +1,30 @@
-# [Image cutter by points](https://github.com/LucasdRossi/image-cutter)
+# Image cutter by points
 
 ### Dependences:
 - **numpy** and **OpenCV**
 
-### How to use:
-First you will need to install de dependences with [pip](https://pypi.org/project/pip/).
+### Getting started:
+First you will need to install de dependences with [pip](https://pypi.org/project/pip/)
 `$ pip install numpy && pip install opencv-python`
 
-Then you just need to run the `main.py` file in the application with the path of the image that you want to use. `:)`
-`$ python ./main.py -img <path>`
+There are  three ways to start the application:  
+1. Using an image:  
+`$ python ./main.py -img <path_to_image>`  
+2. Using your webcam:  
+`$ python ./main.py -v CAM`  
+3. Using a video:  
+`$ python ./main.py -v <path_to_video>`    
 
-With the image open you can press `s` and a new tab will be opened for you to select the points you want. Then ou need to press `c` to cut the image.
-To quit the application simply press `q` any time.
+### How to use:
+With the image open you can press **`s`** and a new tab will be opened for you to select the points you want. Then ou need to press **`c`** to cut the image
+To quit the application simply press **`q`** any time
 
 ### Examples:
+**Image**
 ![](./examples/1.png)
+
+**Image with the selected points**
 ![](./examples/2.png)
+
+**Cropped image**
 ![](./examples/3.png)
--- main.py
+++ main.py
@@ -2,18 +2,17 @@ from argparse import ArgumentParser
 import numpy as np
 from cv2 import cv2
 
-all_points = []
-
 
 def build_args():
     parser = ArgumentParser()
     parser.add_argument("-img", "--image_path", required=False,
-                        type=str, help="path to the image", default="./image.jpg")
+                        type=str, help="path to the image", default="./resources/image.jpg")
+    parser.add_argument("-v", "--video", required=False,
+                        type=str, help="type CAM to webcam or the path to the video", default=None)
     return parser
 
 
 def get_mouse_points(event, x, y, flags, param):
-    global all_points
     if event == cv2.EVENT_LBUTTONDOWN:
         points = (x, y)
         all_points.append(points)
@@ -21,7 +20,6 @@ def get_mouse_points(event, x, y, flags, param):
 
 
 def get_points():
-    global all_points
     min_x = all_points[0][0]
     max_x = all_points[0][0]
     min_y = all_points[1][1]
@@ -39,17 +37,47 @@ def get_points():
     return min_x, max_y, max_x, min_y
 
 
+def get_final_img():
+    min_x, max_y, max_x, min_y = get_points()
+    square_img = img[min_y:max_y, min_x:max_x]
+
+    mask = np.zeros(square_img.shape, square_img.dtype)
+    cv2.fillPoly(
+        img_to_cut, [np.asarray(all_points, dtype=np.int32)], (255, 255, 255))
+    mask = img_to_cut[min_y:max_y, min_x:max_x]
+    mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+
+    final = cv2.bitwise_and(square_img, mask)
+    return final
+
+
 if __name__ == '__main__':
+    all_points = []
     args = build_args().parse_args()
-    img_path = args.image_path
-    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
-    img_copy = img.copy()
-    img_to_cut = img.copy()
     selecting_points = False
     selected_points = False
     cropped_img = False
+    get_frame = True
+    video = True if args.video != None else False
+
+    if video:
+        if args.video == 'CAM':
+            cam = cv2.VideoCapture(0)
+        else:
+            cam = cv2.VideoCapture(args.video)
+    else:
+        img_path = args.image_path
+        img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+        img_copy = img.copy()
+        img_to_cut = img.copy()
 
     while True:
+        if video:
+            _, img = cam.read()
+            img_to_cut = img.copy()
+
         cv2.imshow('image', img)
         key = cv2.waitKey(100)
 
@@ -59,6 +87,9 @@ if __name__ == '__main__':
             selecting_points = True
 
         if selecting_points:
+            if get_frame and video:
+                img_copy = img.copy()
+                get_frame = False
             cv2.imshow('select points', img_copy)
 
         if key == 99 and len(all_points) > 2 and selecting_points:  # 'c'
@@ -68,26 +99,18 @@ if __name__ == '__main__':
                 'select points', lambda event, x, y, flags, param: None)
 
         if selected_points:
-            min_x, max_y, max_x, min_y = get_points()
-            square_img = img[min_y:max_y, min_x:max_x]
-
-            mask = np.zeros(square_img.shape, square_img.dtype)
-            cv2.fillPoly(
-                img_to_cut, [np.asarray(all_points, dtype=np.int32)], (255, 255, 255))
-            mask = img_to_cut[min_y:max_y, min_x:max_x]
-            mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-            _, mask = cv2.threshold(mask, 254, 255, cv2.THRESH_BINARY)
-            mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-
-            final = cv2.bitwise_and(square_img, mask)
-
+            final_img = get_final_img()
             cropped_img = True
-            selected_points = False
+            if not video:
+                selected_points = False
 
         if cropped_img:
-            cv2.imshow('cropped image', final)
-            cropped_img = False
+            cv2.imshow('cropped image', final_img)
+            if not video:
+                cropped_img = False
 
         if key == 113:  # 'q'
+            if video:
+                cam.release()
             cv2.destroyAllWindows()
             break
